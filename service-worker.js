64344537b4031eca7250b144fae55930dfc435a1
Himself65 <himself65@outlook.com>
2019-12-12 15:37:11 +0000

    Deploying to gh-pages from dev 645c38dd134c9964d6d5773a56ebbb65a5a9cefd

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cdot.network/precache-manifest.c7c51bb8ca9e624748725bb1cd5e1457.js"
+  "/cdot.network/precache-manifest.315eb345ff88554a91bc08eb47255552.js"
 );
 
 self.addEventListener('message', (event) => {
